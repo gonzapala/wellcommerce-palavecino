3868fc43a1207e9d2efe250ac45aeee59971049b
Gonzalo Palavecino <GRPalavecino@teco.com.ar>
2022-05-16 22:41:38 -0300

    ItemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,12 +1,18 @@
 //@ts-check
 import React, { useState } from "react";
 
-export default function ItemCount({ stock, initial }) {
+export default function ItemCount({ stock, initial, onAdd }) {
 	const [itemCount, setItemCount] = useState(initial);
 	
 	const handleChange = (event) => {
-		console.log(event.value);
-		setItemCount(event.value)
+		console.log('event.value: ', event.value, stock);
+		if(Number(event.value) >= 0 && Number(event.value) <= stock){
+			setItemCount(event.value)
+		}else{
+			console.log('No está en el rango.');
+			event.value = initial;
+			setItemCount(initial)
+		}
 		// if(event.value == ''){
 		// 	console.log('vacio');
 		// 	setItemCount(initial)
@@ -14,21 +20,63 @@ export default function ItemCount({ stock, initial }) {
 	}
 	
 	function addElement(){
-		setItemCount( itemCount + 1);
+		console.log(itemCount)
+		if(itemCount >= stock){
+			setItemCount(stock)
+		}else{
+			setItemCount(  Number(itemCount) + 1);
+		}
 	}
 	
 	const removeElement = () => {
-		setItemCount( itemCount - 1);
+		if(itemCount <= 0){
+			setItemCount(0)
+		}else{
+			setItemCount(  Number(itemCount) - 1);
+		}
+
+	}
+
+	function add(){
+		onAdd(itemCount);
+	}
+
+	function remove(){
+		setItemCount(0)
+		onAdd(0);
 	}
 	return (
 		<>
-			{itemCount}
-			<button className="btn btn-info" onClick={removeElement} > - </button>
-			<input  type="number" min="0" 
-				value={itemCount}
-				onInput={evt => handleChange(evt.target)}/>
-			<button className="btn btn-info" onClick={() => addElement()}> + </button>
-			<button className="btn btn-info" onClick={() => addElement()}> Agregar al Carrito </button>
+			<div>
+				<p className="badge bg-secondary">Solo quedan {stock} disponibles</p><br></br>
+				<img src="https://static.dafiti.com.ar/p/shaffe-co-2290-970609-1-product.jpg" 
+					style={{width: 250}} alt="" />
+				<p>
+					Remera Roja <span className="badge rounded-pill text-bg-light	">Talle L</span>
+					<br></br>
+				</p>
+			</div>
+			<div>
+				<button className="btn btn-info"
+					onClick={removeElement}
+					disabled={itemCount <= 0}> - </button>
+
+				<input  type="number" min="0" max={stock}
+					value={itemCount}
+					onInput={evt => handleChange(evt.target)}/>
+
+				<button className="btn btn-info" 
+					onClick={addElement}
+					disabled={itemCount >= stock}> + </button>
+			</div>
+
+			<button className="btn btn-info" 
+				onClick={() => add()}
+				disabled={itemCount === 0 || stock === 0}
+			> Agregar </button>
+			<button className="btn btn-outline-info" 
+				onClick={remove}> Quitar </button>
+			
 		</>
 	);
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,28 +1,47 @@
 //@ts-check
-import React from 'react'
+import React, { useState } from 'react'
 import ItemCount from './ItemCount';
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function ItemListContainer() {
-  let products = [
-    {
-      id: 1001,
-      name: 'Prod 1',
-      text: 'Text ',
-      stock: 10
-      // price: 1280,25
-    },
-    {
-      id: 1001,
-      name: 'Prod 1',
-      text: 'Text ',
-      // price: 1280,25
-      stock: 20
-    }
-  ];
+  // let products = [
+  //   {
+  //     id: 1001,
+  //     name: 'Prod 1',
+  //     text: 'Text ',
+  //     stock: 10
+		// img: 'https://static.dafiti.com.ar/p/shaffe-co-4256-500609-1-product.jpg'
+  //     // price: 1280,25
+  //   },
+  //   {
+  //     id: 1001,
+  //     name: 'Prod 1',
+  //     text: 'Text ',
+  // img: 'https://static.dafiti.com.ar/p/shaffe-co-2290-970609-1-product.jpg'
+  //     // price: 1280,25
+  //     stock: 20
+  //   }
+  // ];
+  const [cart, setCart] = useState(0)
+  
+  function onAdd(cant){
+    console.log('onAdd ', cant)
+    setCart(cant);
+  }
  
   return (
     <div style={{padding: 50}}>
-        <ItemCount stock="5" initial="1"/>
+        <ItemCount stock="5" initial="1" onAdd={onAdd} />
+		<hr />
+        <p className="text-secondary">
+			
+          	<span className={`badge rounded-pill ${ cart > 0 ? "bg-success" : "bg-secondary"}`}>
+				{cart > 0 && <ShoppingCartIcon />}
+				{cart === 0 && <ShoppingCartOutlinedIcon />}
+			  	{cart}
+			</span> 
+			&nbsp; prendas en el carrito</p>
     </div>
   )
 }
